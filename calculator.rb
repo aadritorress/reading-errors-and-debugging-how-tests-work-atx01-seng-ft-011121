491b0e8ee07f45a61e709a941adcbd011a2ee881
Adri Torres <adriana.amaral@msn.com>
2020-11-16 02:43:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+require '../calculator.rb'
+
+ describe 
+  do first_number = 5
+second_number = 5
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+end
